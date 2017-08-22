5e82f6b425e95d2f3e6bab931485177ad184785b
chch1019 <30661021+chch1019@users.noreply.github.com>
2017-08-23 05:00:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+//change the text of main text div
+
+var element = getElememtById('main-text');
+
+element.innerHTML = 'New value';
+
+// move the Image]
+
+var img = getElementById('chaitu');
+img.onclick = function (){
+    
+    img.style.marginLeft = '100px';
+    
+};
