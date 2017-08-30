0dc575f99b98511959e6bdab039d801e41b4af76
chch1019 <30661021+chch1019@users.noreply.github.com>
2017-08-30 06:14:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,14 @@
-console.log('Loaded!');
-//change the text of main text div
-
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New value';
-
-// move the Image]
-
-var img = document.getElementById('chaitu');
-marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft+ "px";
-    
-} 
-img.onclick = function (){
-    var interval = setInterval (moveRight, 25);
+//counter code
+counter =0;
+var button = document.getElementById('counter');
+button.onClick = function(){
+   
+   // Make a request to the counter end point
+   
+   //capture the response and store it in a variable
+   
+   // Render the variable in a correct span
+   counter = counter +1;
+   var span= getElementById('count');
+   span.innerHTML = counter.toString();
 };
